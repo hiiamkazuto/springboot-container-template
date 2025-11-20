33989d5977e9f57c6b2524855c4d23c3487cbb1f
MIURA, Kazuto <hiiamkazuto@gmail.com>
2025-11-20 10:15:11 +0900

    refactor: simplify main class name to Application

--- src/main/java/com/example/springboot_container_template/Application.java
+++ src/main/java/com/example/springboot_container_template/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringbootContainerTemplateApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringbootContainerTemplateApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
