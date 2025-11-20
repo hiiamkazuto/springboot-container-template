69416d0adf13bcbf955405648501679503c07d5a
MIURA, Kazuto <hiiamkazuto@gmail.com>
2025-11-20 10:10:04 +0900

    first commit

---
+++ src/main/java/com/example/springboot_container_template/SpringbootContainerTemplateApplication.java
@@ -0,0 +1,13 @@
+package com.example.springboot_container_template;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootContainerTemplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootContainerTemplateApplication.class, args);
+	}
+
+}
